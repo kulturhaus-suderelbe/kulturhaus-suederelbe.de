2e9af037f28083c955d4550e01ab7e9c3cf6f433
fricklerhandwerk <valentin@fricklerhandwerk.de>
2017-07-11 13:58:42 +0200

    remove migration artifacts

--- script/markdown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# transform to markdown
-find . -name "*.html" | parallel pandoc -t markdown --atx-headers -i {} -o {.}.md
-# replace backslash linebreaks by double space
-for i in `find -name "*.md"`; do sed -i 's/\\/ /g' $i; done
